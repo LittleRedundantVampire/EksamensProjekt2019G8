a4ca229120e6f22580ec96fbca8425c0c757bed3
SigneSigurd <namie_orihara@hotmail.com>
2019-12-02 11:04:25 +0100

    Klasser 2

---
+++ Gruppe8Eksamensprojekt2019/Gruppe8Eksamensprojekt2019/LevelTwo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruppe8Eksamensprojekt2019
+{
+    class LevelTwo
+    {
+        private void LoadContent(ContentManager content)
+        {
+
+        }
+        private void ChangeLevel()
+        {
+
+        }
+
+        private void LevelSetUp()
+        {
+
+        }
+    }
+}
