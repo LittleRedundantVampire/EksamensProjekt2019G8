31b0f0b55d226d15c0bb2924a2932eb6b1102cd5
thecatmiaw <58418706+thecatmiaw@users.noreply.github.com>
2019-12-02 11:10:29 +0100

    Henriks klasser(GameObject, BreakableObject, Vase)

---
+++ Gruppe8Eksamensprojekt2019/Gruppe8Eksamensprojekt2019/BreakableObject.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruppe8Eksamensprojekt2019
+{
+    abstract class BreakableObject : GameObject
+    {
+        protected bool isBroken;
+
+        protected abstract void Break();
+
+        protected override void Update(GameTime gameTime)
+        {
+
+        }
+
+
+        protected override void Load(ContentManager Content)
+        {
+
+        }
+
+
+    }
+}
---
+++ Gruppe8Eksamensprojekt2019/Gruppe8Eksamensprojekt2019/GameObject.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruppe8Eksamensprojekt2019
+{
+    abstract class GameObject
+    {
+        private Rectangle collisionBox;
+
+        protected Texture2D sprite;
+        protected byte currentIndex;
+        protected float fps;
+        protected Vector2 position;
+        protected Vector2 velocity;
+        protected int speed;
+        protected bool hasShadow;
+
+        public bool HasShadow
+        {
+            get { return hasShadow;  }
+        }
+
+        public Rectangle CollisionBox
+        {
+            get { return collisionBox; }
+        }
+
+
+        protected virtual void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+        protected abstract void Update(GameTime gameTime);
+
+        protected abstract void Load(ContentManager Content);
+
+        protected virtual void CheckCollision(GameObject gameObject)
+        {
+
+        }
+
+        protected virtual void OnCollision(GameObject other)
+        {
+
+        }
+
+        protected virtual void Move()
+        {
+
+        }
+
+    }
+}
