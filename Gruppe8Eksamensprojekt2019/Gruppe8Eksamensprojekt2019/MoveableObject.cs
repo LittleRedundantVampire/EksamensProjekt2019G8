7a959f5731f87b075de03798de2c9fd2a6f5eabe
SigneSigurd <namie_orihara@hotmail.com>
2019-12-02 10:59:26 +0100

    Klasser

---
+++ Gruppe8Eksamensprojekt2019/Gruppe8Eksamensprojekt2019/MoveableObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruppe8Eksamensprojekt2019
+{
+    class MoveableObject : GameObject
+    {
+        protected bool isCollidingMO;
+
+        protected void Push()
+        {
+
+        }
+    }
+}
---
+++ Gruppe8Eksamensprojekt2019/Gruppe8Eksamensprojekt2019/Player.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruppe8Eksamensprojekt2019
+{
+    class Player : Character
+    {
+        private int regeneration;
+        private SoundEffect playerAttackSound;
+
+        public Player(string name, int health, Vector2 position)
+        {
+
+        }
+
+        private void Move()
+        {
+
+        }
+
+        private void HandleInput()
+        {
+
+        }
+
+        private void InvincibleTimer(GameTime gameTime)
+        {
+
+        }
+
+        private void Attack()
+        {
+
+        }
+
+        private void SuckAttack()
+        {
+
+        }
+
+        private void UseAbility(Ability ability)
+        {
+
+        }
+
+        private void UpdateInventory()
+        {
+
+        }
+
+        private void OnCollision(GameObject other)
+        {
+
+        }
+    }
+}
