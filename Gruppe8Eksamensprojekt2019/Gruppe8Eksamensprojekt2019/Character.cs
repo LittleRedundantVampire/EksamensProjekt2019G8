ffbab1e033d736c5d1b21c41cabccc36aef419ee
KuMoJo <KuMoJo@users.noreply.github.com>
2019-12-02 11:17:38 +0100

    Boss og Character tilføjet

---
+++ Gruppe8Eksamensprojekt2019/Gruppe8Eksamensprojekt2019/Character.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruppe8Eksamensprojekt2019
+{
+    enum Ability { Sunblock, Fire, Ice };
+
+    abstract class Character
+    {
+        protected int health;
+        protected int damage;
+        protected string name;
+
+
+        public virtual void UpdateHealth(int health, int amount)
+        {
+
+        }
+
+        public virtual void Attack()
+        {
+
+        }
+
+        public virtual void UseAbility(Ability ability)
+        {
+
+        }
+
+        public virtual void Speak()
+        {
+
+        }
+    }
+}
