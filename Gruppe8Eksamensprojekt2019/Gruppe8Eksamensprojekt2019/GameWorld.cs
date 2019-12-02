508d2d748788261a42472ebaaf55f8ed9ec22099
SigneSigurd <namie_orihara@hotmail.com>
2019-12-02 13:44:14 +0100

    GameWorld

--- Gruppe8Eksamensprojekt2019/Gruppe8Eksamensprojekt2019/GameWorld.cs
+++ Gruppe8Eksamensprojekt2019/Gruppe8Eksamensprojekt2019/GameWorld.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.Collections.Generic;
 
 namespace Gruppe8Eksamensprojekt2019
 {
@@ -11,6 +13,17 @@ namespace Gruppe8Eksamensprojekt2019
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+
+        private List<GameObject> playerAbilities = new List<GameObject>();
+        private List<GameObject> shadows = new List<GameObject>();
+        private List<GameObject> gameObjects = new List<GameObject>();
+        private List<GameObject> newObjects = new List<GameObject>();
+        private List<GameObject> deleteObjects = new List<GameObject>();
+
+        private Song currentMusic;
+        private byte currentLevel;
+        protected Texture2D collisionTexture;
+
 
         public GameWorld()
         {
@@ -78,6 +91,11 @@ namespace Gruppe8Eksamensprojekt2019
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
+        }
+
+        private void DrawCollisionBox(GameObject gameObject)
+        {
+
         }
     }
 }
