95307d37b582af517299f8bd9814baef808794a9
Beter <peterriemerbjorn@gmail.com>
2019-12-02 11:04:11 +0100

    SkrivKlasserIndBeter

---
+++ Gruppe8Eksamensprojekt2019/Gruppe8Eksamensprojekt2019/Chest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Gruppe8Eksamensprojekt2019
+{
+    class Chest
+    {
+        private bool isOpen;
+
+        public Chest(Texture2D sprite, Vector2 position, bool hasShadow)
+        {
+
+        }
+        private void OpenChest()
+        {
+
+        }
+        private void GivePrompt()
+        {
+
+        }
+    }
+}
---
+++ Gruppe8Eksamensprojekt2019/Gruppe8Eksamensprojekt2019/Door.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Gruppe8Eksamensprojekt2019
+{
+    class Door
+    {
+        private bool isOpen;
+
+        public Door(Texture2D sprite, Vector2 position, bool hasShadow)
+        {
+
+        }
+        private void OpenDoor()
+        {
+
+        }
+        private void GivePrompt()
+        {
+
+        }
+    }
+}
